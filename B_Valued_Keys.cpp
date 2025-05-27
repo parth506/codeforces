8fdb401d4c3acd676f7236afab4952dd0a3cfde3
parth506 <parthagrawal4675@gmail.com>
2025-05-27 11:58:58 +0530

    2.commit [ new problem added ]

---
+++ B_Valued_Keys.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string x, y;
+    cin >> x >> y;
+    int n = x.size();
+    // min(x[i],z[i])=y[i]
+    string z;
+    z = x;
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (y[i] == x[i])
+        {
+            z[i] = y[i] + 1;
+        }
+        else if (y[i] == 'b' && x[i] == 'a')
+        {
+            cout << -1 << endl;
+            return 0;
+        }
+        else if (y[i] != x[i])
+        {
+            z[i] = y[i];
+        }
+    }
+    cout << z << endl;
+}
