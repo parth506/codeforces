0012f77108c6f2bbfb13b0d01c3b14bee422ab78
parth506 <parthagrawal4675@gmail.com>
2025-06-07 22:42:46 +0530

    string operation added

---
+++ string_learn.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s;
+    cin >> s;
+    cout << s << endl;
+    string p = "parth agrawal";
+    cout << p << endl;
+    cout << s + p << endl;
+}
